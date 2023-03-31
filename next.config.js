fa0654d8cd82515f20f62861b6bc455069151c9d
Hari <92938055+NateNear@users.noreply.github.com>
2023-03-31 18:36:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  images: {
+    domains: ['source.unsplash.com'],
+  },
+}
